cf9ea420e083012beb4b275f9a8f81af73bb1dde
Jocelino Neto <netojocelino@inventione.com.br>
2020-04-19 01:51:25 -0300

    Nova linha em EOF

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 
 import NavBar from '../NavBar';
 
 export default function Header() {
     return (
         <div className="bg-gray-200">
-            <h1>
-                <img src="/logo.png" alt="Olá FSL!" className="h-24 mx-auto py-4" />
-            </h1>
+            <Link href="/">
+                <a>
+                    <h1>
+                        <img src="/logo.png" alt="Olá FSL!" className="h-24 mx-auto py-4" />
+                    </h1>
+                </a>
+            </Link>
 
             <NavBar />
         </div>
